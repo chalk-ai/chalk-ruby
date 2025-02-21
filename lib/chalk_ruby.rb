f17e5d2a63104a2e1d94bb89f3aa087e04a38f7b
Andrew Blackmon <andrew@found.com>
2025-02-20 19:00:08 -0600

    Missing require for grpc client

--- lib/chalk_ruby.rb
+++ lib/chalk_ruby.rb
@@ -7,6 +7,7 @@ module ChalkRuby
   require 'chalk_ruby/http/response'
   require 'chalk_ruby/http/http_requester_chalk'
   require 'chalk_ruby/client'
+  require 'chalk_ruby/grpc_client'
   require 'chalk_ruby/error'
   require 'chalk_ruby/logger_helper'
 end
